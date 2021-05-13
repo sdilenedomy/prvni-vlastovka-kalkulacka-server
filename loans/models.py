1f061d0216ffa22cbf18a7583012a2a42c00db94
kokolem <vitek.peterk@gmail.com>
2021-05-14 00:33:28 +0200

    add currency, fix bugs

--- loans/models.py
+++ loans/models.py
@@ -6,6 +6,17 @@ from django.utils.translation import gettext_lazy as _, ngettext_lazy, gettext
 class Loan(models.Model):
     amount = models.IntegerField(verbose_name=_("Loan amount"))
 
+    CURRENCY_CHOICES = [
+        ('Kč', 'Kč'),
+        ('EUR', 'EUR')
+    ]
+    currency = models.CharField(
+        max_length=3,
+        verbose_name=_('currency'),
+        choices=CURRENCY_CHOICES,
+        default='Kč'
+    )
+
     INTEREST_TYPE_OPTIONS = [
         ('end', _('repayment of both prinicipal and interest at the end')),
         ('yearly', _('yearly repayment of interest, principal at the end'))
@@ -16,7 +27,7 @@ class Loan(models.Model):
         verbose_name=_('interest repayment type')
     )
 
-    INTEREST_OPTIONS = [0, 0.5, 1, 1.5, 2, 2.5, 3]
+    INTEREST_OPTIONS = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
     interest = models.FloatField(
         choices=map(lambda i: (i, f"{str(i)} %"), INTEREST_OPTIONS),
         verbose_name=_('interest')
@@ -28,7 +39,6 @@ class Loan(models.Model):
         '%d years',
     )
     duration = models.IntegerField(
-
         choices=map(lambda i, dur_str=DURATION_STRING: (i, dur_str % i), DURATION_OPTIONS),
         verbose_name=_('duration')
     )
@@ -44,7 +54,8 @@ class LoanOffer(Loan):
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
         verbose_name=_('responsible person'),
-        null=True
+        null=True,
+        blank=True,
     )
 
     class Meta:
