1a9c116e4b19b9ed6086986fddbd3f36e365fdbd
A <64016422+lexik-wtf@users.noreply.github.com>
2022-09-29 12:26:23 +0200

    interest up to 5%

--- loans/models.py
+++ loans/models.py
@@ -27,7 +27,7 @@ class Loan(models.Model):
         verbose_name=_('interest repayment type')
     )
 
-    INTEREST_OPTIONS = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
+    INTEREST_OPTIONS = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
     interest = models.FloatField(
         choices=map(lambda i: (i, f"{str(i)} %"), INTEREST_OPTIONS),
         verbose_name=_('interest')
